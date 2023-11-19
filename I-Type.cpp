f9f9abefe6c1ce3a4ba46b9b33c138132daa0f5c
KDLeslie <thekaidenater@gmail.com>
2023-11-18 21:24:38 -0700

    Fixed I-Type file

--- I-Type.cpp
+++ I-Type.cpp
@@ -5,104 +5,101 @@
 
 #include "instructions.h"
 
-// Initializes memory contents
-Memory::Memory(int var)
-{
-    mem.resize(var);
-    regi["$zero"] = 0;
-    regi["$at"] = 0;
-    regi["$v0"] = 0;
-    regi["$v1"] = 0;
-    regi["$a0"] = 0;
-    regi["$a1"] = 0;
-    regi["$a2"] = 0;
-    regi["$a3"] = 0;
-    regi["$t0"] = 0;
-    regi["$t1"] = 0;
-    regi["$t2"] = 0;
-    regi["$t3"] = 0;
-    regi["$t4"] = 0;
-    regi["$t5"] = 0;
-    regi["$t6"] = 0;
-    regi["$t7"] = 0;
-    regi["$s0"] = 0;
-    regi["$s1"] = 0;
-    regi["$s2"] = 0;
-    regi["$s3"] = 0;
-    regi["$s4"] = 0;
-    regi["$s5"] = 0;
-    regi["$s6"] = 0;
-    regi["$s7"] = 0;
-    regi["$t8"] = 0;
-    regi["$t9"] = 0;
-    regi["$k0"] = 0;
-    regi["$k1"] = 0;
-    regi["$gp"] = 0;
-    regi["$sp"] = 0;
-    regi["$fp"] = 0;
-    regi["$ra"] = 0;
-}
-
-// Displays a help message for the user
-void help()
-{
-    std::cout << "Enter one of the following keys..." << std::endl;
-    std::cout << "n: Execute next instruction" << std::endl;  
-    std::cout << "i: Displays the next instruction to be executed" << std::endl;  
-    std::cout << "m <address>: Displays the word at that memory address" << std::endl;    
-    std::cout << "h: Displays this help message again" << std::endl;
-    std::cout << "e: Exit the program" << std::endl;
-}
-
-int main()
-{
-    Memory m(0x0FFFFFFF);
-    std::cout << "Welcome to the MIPS Debugger." << std::endl;
-    help();
-    std::string s;
-    std::cin >> s;
-    while (s != "e")
-    {
-        if (s == "n")
-        {
-            // For testing
-            // m.regi["$s1"] = 1;
-            m.regi["$s0"] = 10;
-
-            // Fetch and execute next instruction
-            addu(m, "$s0", "$s1", "$s2");
-            std::cout << "Executed Instruction: ";
-            std::cout << "addu $s2, $s0, $s1" << std::endl;
-
-            std::cout << "Registers:" << std::endl;
-            int column = 0;
-            // Display registers in 8 rows of 4 columns
-            for (std::map<std::string, int>::iterator i = m.regi.begin(); i != m.regi.end(); i++)
-            {               
-                std::cout << i->first << ": " << i->second << " ";
-                column++;               
-                if (column == 4)
-                {
-                     // Start a new row
-                     std::cout << std::endl;
-                    column = 0;                    
-                } 
-            }
-        }
-        else if (s == "i")
-        {
-           std::cout << "addu $s2, $s0, $s1" << std::endl;
-        }
-        else if(s == "h")
-        {
-            help();
-        }
-        else if (s == "m")
-        {
-            int i;
-            std::cin >> i;
-            std::cout << m.mem[i/4] << std::endl;
-        }
-        std::cin >> s;
+void lw(Memory& mips, std::string rs, std::string rt, int32_t offset){
+    uint32_t addr = offset + mips.regi[rs];
+    if (addr%4 != 0){
+        return;
     }
+    mips.regi[rt] = mips.mem[addr/4];
+    mips.pc += 1;
+
+}
+
+void lb(Memory& mips, std::string rs, std::string rt, int32_t offset){
+    uint32_t addr = offset + mips.regi[rs];
+    int32_t word = mips.mem[addr/4];
+    if (addr%4 == 0){
+        // Mips is little endian
+        mips.regi[rt] =  (int8_t)((word & 0xFF000000) >> 24);
+    }
+    if (addr%4 == 1){
+        mips.regi[rt] =  (int8_t)((word & 0x00FF0000) >> 16);
+    }
+    if (addr%4 == 2){
+        mips.regi[rt] =  (int8_t)((word & 0x0000FF00) >> 8);
+    }
+    if (addr%4 == 3){
+        mips.regi[rt] =  (int8_t)((word & 0x000000FF));
+    }
+    mips.pc += 1;
+
+}
+
+void lbu(Memory& mips, std::string rs, std::string rt, int32_t offset){
+    uint32_t addr = offset + mips.regi[rs];
+    int32_t word = mips.mem[addr/4];
+    if (addr%4 == 0){
+        // Mips is little endian
+        mips.regi[rt] =  (u_int8_t)((word & 0xFF000000) >> 24);
+    }
+    if (addr%4 == 1){
+        mips.regi[rt] =  (u_int8_t)((word & 0x00FF0000) >> 16);
+    }
+    if (addr%4 == 2){
+        mips.regi[rt] =  (u_int8_t)((word & 0x0000FF00) >> 8);
+    }
+    if (addr%4 == 3){
+        mips.regi[rt] =  (u_int8_t)((word & 0x000000FF));
+    }
+    mips.pc += 1;
+
+}
+
+void sw(Memory& mips, std::string rs, std::string rt, int32_t offset){
+    uint32_t addr = offset + mips.regi[rs];
+    if (addr%4 != 0){
+        return;
+    }
+    mips.mem[addr/4] = mips.regi[rt];
+    mips.pc += 1;
+}
+
+void addi(Memory& mips, std::string rs, std::string rt, int32_t imm){
+    mips.regi[rt] = mips.regi[rs] + imm;
+    mips.pc += 1;
+}
+
+void addiu(Memory& mips, std::string rs, std::string rt, uint32_t imm){
+    mips.regi[rt] = mips.regi[rs] + imm;
+    mips.pc += 1;
+}
+
+void slti(Memory& mips, std::string rs, std::string rt, int32_t imm){
+    mips.regi[rt] = mips.regi[rs] < imm;
+    mips.pc += 1;
+}
+
+void sltiu(Memory& mips, std::string rs, std::string rt, uint32_t imm){
+    mips.regi[rt] = mips.regi[rs] < imm;
+    mips.pc += 1;
+}
+
+void andi(Memory& mips, std::string rs, std::string rt, uint32_t imm){
+    mips.regi[rt] = mips.regi[rs] & imm;
+    mips.pc += 1;
+}
+
+void ori(Memory& mips, std::string rs, std::string rt, uint32_t imm){
+    mips.regi[rt] = mips.regi[rs] | imm;
+    mips.pc += 1;
+}
+
+void xori(Memory& mips, std::string rs, std::string rt, uint32_t imm){
+    mips.regi[rt] = mips.regi[rs] ^ imm;
+    mips.pc += 1;
+}
+
+void lui(Memory& mips, std::string rt, int32_t imm){
+    mips.regi[rt] = imm << 16;
+    mips.pc += 1;
 }
